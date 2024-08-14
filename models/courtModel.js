263d78e23bf0fc89b76ee11b70f68dd4e3ee7b37
bherovone <pargi.bherov.businessone@gmail.com>
2024-08-14 15:53:32 +0530

    Update courtModel.js

--- models/courtModel.js
+++ models/courtModel.js
@@ -6,11 +6,16 @@ const courtSchema = new Schema(
     name: { type: String, required: true },
     location: {
       address: { type: String, required: true },
-      latitude: { type: Number, required: true },
-      longitude: { type: Number, required: true }
+      coordinates: {
+        type: [Number], // [longitude, latitude]
+        required: true,
+      }
     }
   },
   { timestamps: true }
 );
+
+// Create a 2dsphere index on the coordinates field
+courtSchema.index({ location: "2dsphere" });
 
 module.exports = mongoose.model("Court", courtSchema);
