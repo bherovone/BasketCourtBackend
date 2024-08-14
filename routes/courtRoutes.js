658d45b066d58c7264c6dc50f363951eabac5b82
bherovone <pargi.bherov.businessone@gmail.com>
2024-08-14 15:54:08 +0530

    Update courtRoutes.js

--- routes/courtRoutes.js
+++ routes/courtRoutes.js
@@ -3,7 +3,8 @@ const { auth, checkAdmin } = require("../middleware/auth");
 const courtController = require("../controllers/courtController");
 const courtRouter = express.Router();
 
-courtRouter.post("/addcourt", auth, checkAdmin, courtController.addCourt);
+courtRouter.get("/check", auth, courtController.checkCourtExist);
+courtRouter.post("/addcourt", auth, courtController.addCourt);
 courtRouter.get("/getallcourts", auth, courtController.getAllCourts);
 courtRouter.get("/getcourt/:id", auth, courtController.getCourtById);
 courtRouter.put("/updatecourt/:id", auth, checkAdmin, courtController.updateCourt);
