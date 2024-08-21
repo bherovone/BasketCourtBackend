9414bab0daa2cf7821dcda5275a319e7c40fb97a
bherovone <pargi.bherov.businessone@gmail.com>
2024-08-21 12:43:12 +0530

    Add files via upload

---
+++ controllers/locationController.js
@@ -0,0 +1,27 @@
+const Location = require("../models/locationModel");
+
+// Function to create a location record
+const createlocation = async (req, res) => {
+  try {
+    const data = req.body; // Assuming data comes from the request body
+    const newLocation = new Location(data);
+    await newLocation.save();
+    console.log("Location record saved:", newLocation);
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "Location recorded successfully",
+        location: newLocation,
+      });
+  } catch (error) {
+    console.error("Error saving location record:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Error saving location record" });
+  }
+};
+
+module.exports = {
+  createlocation,
+};
