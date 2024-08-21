60321315527499cad43ceb4cf394ae6868819e50
bherovone <pargi.bherov.businessone@gmail.com>
2024-08-21 14:09:15 +0530

    Add files via upload

---
+++ controllers/eventController.js
@@ -0,0 +1,27 @@
+const Event = require('../models/eventModel');
+
+const createEvent = async (req, res) => {
+  try {
+    const { user_id, type, event } = req.body;
+    const newEvent = new Event({
+      user_id, // Add user_id here
+      type,
+      event,
+      timestamp: new Date(),
+    });
+    await newEvent.save();
+    console.log("Event record saved:", newEvent);
+    res.status(201).json({
+      success: true,
+      message: 'Event recorded successfully',
+      event: newEvent,
+    });
+  } catch (error) {
+    console.error('Error saving event record:', error);
+    res.status(500).json({ success: false, message: 'Error saving event record' });
+  }
+};
+
+module.exports = {
+  createEvent,
+};
