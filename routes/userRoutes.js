fe9245b2767ef0422159ab55d34fe72a73cce083
bherovone <pargi.bherov.businessone@gmail.com>
2024-10-22 17:04:09 +0530

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,14 @@ userRouter.get("/getuser/:id", auth, userController.getuser);
 userRouter.get("/getallusers", auth, checkAdmin, userController.getallusers); // Only admin can get all users
 userRouter.post("/adduser", auth, checkAdmin, userController.adduser); // Only admin can get all users
 userRouter.post("/login", userController.login);
-userRouter.post("/register", userController.register);
+userRouter.post("/signup", userController.signup);
 userRouter.put("/updateprofile", auth, userController.updateprofile);
 userRouter.delete("/deleteuser", auth, checkAdmin, userController.deleteuser); // Only admin can delete a user
+
+userRouter.post("/auth/send-otp", userController.sendOtp); // Send OTP for verification
+userRouter.post("/auth/verify-otp", userController.verifyOtp); // Verify OTP for login or signup
+userRouter.post("/auth/forgot-password", userController.forgotPassword); // Forgot password process
+userRouter.post("/auth/reset-password", userController.resetPassword); // Reset password using token
+
 
 module.exports = userRouter;
